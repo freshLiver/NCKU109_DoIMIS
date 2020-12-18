925ef3bb06f2751d7d0e4c615b2cd250bd8ab600
freshLiver <freshliver.cys@gmail.com>
2020-12-19 03:49:40 +0800

    date monospace & bundle keywords to OptSearch

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,12 +33,13 @@ public class MainActivity extends AppCompatActivity {
         this.BntOptSearch = findViewById(R.id.BtnOptSearch);
         this.BtnPlayHistory = findViewById(R.id.BtnPlayHistory);
         this.ETKeywords = findViewById(R.id.ETKeywords);
+
         this.TVHistoryDate = findViewById(R.id.TVHistoryDate);
         this.TVHistoryContent = findViewById(R.id.TVHistoryContent);
 
         // ! button clicked event
-        this.BntOptSearch.setOnClickListener(new MainButtonsMap());
-        this.BtnPlayHistory.setOnClickListener(new MainButtonsMap());
+        this.BntOptSearch.setOnClickListener(new MainButtonsMap(this));
+        this.BtnPlayHistory.setOnClickListener(new MainButtonsMap(this));
 
         // ! history content clicked event
         this.TVHistoryContent.setOnClickListener(v -> NewHistory(false));
@@ -47,22 +49,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * 從 this.ETKeywords 取得 keywords
+     *
+     * @return
+     */
+    public String getKeywords() {
+        return this.ETKeywords.getText().toString();
+    }
+
+    /**
      * 歷史上的今天相關 method 以及 variables
      */
     protected ArrayList<String[]> historyEvents;
     protected int iHistory;
 
     private void InitHistory() {
-        iHistory = 0;
-        historyEvents = null;
+        this.iHistory = 0;
+        this.historyEvents = null;
 
         // TODO get history event list
-        historyEvents = new ArrayList<>();
-        historyEvents.add(new String[]{"8787", "8787878787"});
-        historyEvents.add(new String[]{"666", "77777"});
+        this.historyEvents = new ArrayList<>();
+        this.historyEvents.add(new String[]{"8787", "8787878787"});
+        this.historyEvents.add(new String[]{"666", "77777"});
 
         // shuffle history events
-        Collections.shuffle(historyEvents);
+        Collections.shuffle(this.historyEvents);
 
         // new a history
         NewHistory(true);
@@ -75,24 +86,24 @@ public class MainActivity extends AppCompatActivity {
 
         // if is not init increase history index
         if (isInit == false)
-            iHistory = (iHistory + 1) % historyEvents.size();
+            this.iHistory = (this.iHistory + 1) % this.historyEvents.size();
 
         // get new history
-        if (historyEvents != null) {
-            String dateInfo = String.format("西元 %04s 年 %02d 月 %02d 日"
-                    , historyEvents.get(iHistory)[0]
+        if (this.historyEvents != null) {
+            String dateInfo = String.format("西元 %4s / %02d / %02d "
+                    , this.historyEvents.get(this.iHistory)[0]
                     , today.get(Calendar.MONTH)
                     , today.get(Calendar.DAY_OF_MONTH));
 
-            TVHistoryDate.setText(dateInfo);
-            TVHistoryContent.setText(historyEvents.get(iHistory)[1]);
+            this.TVHistoryDate.setText(dateInfo);
+            this.TVHistoryContent.setText(this.historyEvents.get(this.iHistory)[1]);
         } else {
-            String dateInfo = String.format("西元 YYYY 年 %02d 月 %02d 日"
+            String dateInfo = String.format("西元 YYYY / %02d / %02d "
                     , today.get(Calendar.MONTH)
                     , today.get(Calendar.DAY_OF_MONTH));
 
-            TVHistoryDate.setText(dateInfo);
-            TVHistoryContent.setText("No Event (NULL)");
+            this.TVHistoryDate.setText(dateInfo);
+            this.TVHistoryContent.setText("No Event (NULL)");
         }
         Toast.makeText(this, "下一則", Toast.LENGTH_SHORT).show();
     }
@@ -101,16 +112,30 @@ public class MainActivity extends AppCompatActivity {
 
 class MainButtonsMap implements OnClickListener {
 
+    public MainActivity main;
+
+    public MainButtonsMap(MainActivity main) {
+        this.main = main;
+    }
+
     @Override
     @SuppressLint("NonConstantResourceId")
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.BtnOptSearch:
+                // bring keywords to next activity
                 Intent optSearch = new Intent(v.getContext(), OptionActivity.class);
-                v.getContext().startActivity(optSearch);
+
+                // put keywords into bundle
+                Bundle bundle = new Bundle();
+                bundle.putString(String.valueOf(R.string.bundle_kws_main2opt), this.main.getKeywords());
+
+                // put bundle into intent and start activity
+                optSearch.putExtras(bundle);
+                this.main.startActivity(optSearch);
                 break;
             case R.id.BtnPlayHistory:
-                // TODO
+                // TODO 
                 String msg = "台語播放";
                 Toast.makeText(v.getContext(), msg, Toast.LENGTH_SHORT).show();
                 break;
