4e9588258db931ab9956ab5e0e747974c5983d99
freshLiver <freshliver.cys@gmail.com>
2020-12-19 03:44:21 +0800

    check all setting before do search & get media and types and keywords + date & monospace

--- app/src/main/java/com/example/finalproject/OptionActivity.java
+++ app/src/main/java/com/example/finalproject/OptionActivity.java
@@ -1,46 +1,141 @@
 package com.example.finalproject;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Pair;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
 public class OptionActivity extends AppCompatActivity {
+
+    private String keywords;
+    private ArrayList<String> checkedMedia, checkedTypes;
+    public TextView TVStart, TVEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
 
+        // init arraylist to log checked boxes
+        this.checkedMedia = new ArrayList<>();
+        this.checkedTypes = new ArrayList<>();
+
+        // get keywords from bundle
+        Bundle bundle = this.getIntent().getExtras();
+        this.keywords = bundle.getString(String.valueOf(R.string.bundle_kws_main2opt));
 
         // ! find all components
         Button btnBackToMain = findViewById(R.id.BtnBackToMain);
         Button btnSearch = findViewById(R.id.BtnSearch);
         Button btnSetRangeStart = findViewById(R.id.BtnSetDateStart);
         Button btnSetRangeEnd = findViewById(R.id.BtnSetDateEnd);
+        TVStart = findViewById(R.id.TVOptDateStart);
+        TVEnd = findViewById(R.id.TVOptDateEnd);
+
+        // set default date
+        MyDatePickerListener.MainActivity = this;
 
         // ! button clicked event
-        MyDatePickerListener.MainActivity = this;
         btnSearch.setOnClickListener(new OptSearchButtonsMap(this));
         btnBackToMain.setOnClickListener(new OptSearchButtonsMap(this));
         btnSetRangeStart.setOnClickListener(new OptSearchButtonsMap(this));
         btnSetRangeEnd.setOnClickListener(new OptSearchButtonsMap(this));
+    }
+
+    public Pair<Boolean, String> checkLegalSearch() {
+        // result is a pair <illegal, msg> or <legal, "keywords & before & after">
+        Pair<Boolean, String> result;
+
+        // get start and end date
+        if (OptSearchButtonsMap.start == null || OptSearchButtonsMap.end == null)
+            return new Pair<>(false, "請設定開始以及結束日期");
+        String after = this.TVStart.getText().toString();
+        String before = this.TVEnd.getText().toString();
+
+
+        // get wish types
+        this.checkedTypes.clear();
+        final LinearLayout LLNewsTypes = findViewById(R.id.LLNewsTypes);
+        final ArrayList<View> types = getChildrenWithTag(LLNewsTypes, "TYPES");
+
+        // check all children whose tag is TYPES
+        for (View v : types) {
+            CheckBox box = (CheckBox) v;
+            if (box.isChecked() == true)
+                this.checkedTypes.add(box.getText().toString());
+        }
+        // if no type checked, return false
+        if (checkedTypes.size() == 0)
+            return new Pair<>(false, "請至少句選一個類別");
+
+        // get wish media
+        this.checkedMedia.clear();
+        final LinearLayout LLNewsMedia = findViewById(R.id.LLNewsMedia);
+        final ArrayList<View> media = getChildrenWithTag(LLNewsMedia, "MEDIA");
+
+        // check all children whose tag is MEDIA_PARENT
+        for (View v : media) {
+            CheckBox box = (CheckBox) v;
+            if (box.isChecked() == true)
+                this.checkedMedia.add(box.getText().toString());
+        }
+        // return false if all not checked
+        if (checkedMedia.size() == 0)
+            return new Pair<>(false, "請至少句選一項媒體");
+
+        // everything fine, build query
+        String query = String.format("%s before:%s after:%s", this.keywords, before, after);
+        return new Pair<>(true, query);
+    }
+
+    public Pair<ArrayList<String>, ArrayList<String>> getMediaAndTypes() {
+        return new Pair<>(this.checkedMedia, this.checkedTypes);
+    }
+
+    public ArrayList<View> getChildrenWithTag(View v, String tag) {
+
+        // if not viewgroup, only check tag
+        if ((v instanceof ViewGroup) == false) {
+            // if this view with same tag, return it as list
+            if (v.getTag() != null && (v.getTag().toString().equals(tag))) {
+                ArrayList<View> views = new ArrayList<>();
+                views.add(v);
+                return views;
+            }
+            // if diff tag, just return empty list
+            return new ArrayList<>();
+        }
+
+        // if it is viewgroup, might have many children
+        ArrayList<View> result = new ArrayList<>();
+
+        // convert to viewgroup and check its children views
+        ViewGroup viewGroup = (ViewGroup) v;
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+
+            // get its children with same tag
+            View child = viewGroup.getChildAt(i);
+            result.addAll(getChildrenWithTag(child, tag));
+        }
+
+        // return all children
+        return result;
     }
 }
 
@@ -48,9 +143,9 @@ class OptSearchButtonsMap implements View.OnClickListener {
 
     public static Calendar start = null, end = null;
     @SuppressLint("StaticFieldLeak")
-    private final Activity optActivity;
+    private final OptionActivity optActivity;
 
-    public OptSearchButtonsMap(Activity opt) {
+    public OptSearchButtonsMap(OptionActivity opt) {
         this.optActivity = opt;
     }
 
@@ -59,9 +154,31 @@ class OptSearchButtonsMap implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.BtnSearch:
-                // TODO
-                Intent optSearch = new Intent(this.optActivity, ResultActivity.class);
-                this.optActivity.startActivity(optSearch);
+                // check setting legal or not
+                Pair<Boolean, String> res = optActivity.checkLegalSearch();
+
+                // illegal, show toast for hint
+                if (res.first == false) {
+                    Toast.makeText(optActivity, res.second, Toast.LENGTH_SHORT).show();
+                }
+                // legal, do search
+                else {
+                    Intent optSearch = new Intent(this.optActivity, ResultActivity.class);
+
+                    // show query
+                    Toast.makeText(optActivity, res.second, Toast.LENGTH_SHORT).show();
+                    // use bundle to send settings
+                    Bundle bundle = new Bundle();
+
+                    ArrayList<String> media = this.optActivity.getMediaAndTypes().first;
+                    ArrayList<String> types = this.optActivity.getMediaAndTypes().second;
+                    bundle.putStringArrayList("MEDIA", media);
+                    bundle.putStringArrayList("TYPES", types);
+
+                    // wrap bundle with intent and start result activity
+                    optSearch.putExtras(bundle);
+                    this.optActivity.startActivity(optSearch);
+                }
                 break;
 
             case R.id.BtnBackToMain:
@@ -103,15 +220,6 @@ class OptSearchButtonsMap implements View.OnClickListener {
                 throw new IllegalStateException("Unexpected value: " + v.getId());
         }
     }
-
-    // TODO convert settings to google search keyword
-    public List<String> SettingsToGoogleKeyword(String keyword,
-                                                String dateRange,
-                                                List<String> media,
-                                                List<String> newsTypes) {
-
-        return null;
-    }
 }
 
 class MyDatePickerListener implements DatePickerDialog.OnDateSetListener {
@@ -127,26 +235,21 @@ class MyDatePickerListener implements DatePickerDialog.OnDateSetListener {
     @Override
     @SuppressLint("DefaultLocale")
     public void onDateSet(DatePicker view, int year, int month, int day) {
-
         // get target tv
         TextView TVStart = MainActivity.findViewById(R.id.TVOptDateStart);
         TextView TVEnd = MainActivity.findViewById(R.id.TVOptDateEnd);
 
         // set target TV Text and start or end date
-        try {
-            if (this.ClickedBtnID == R.id.BtnSetDateStart) {
-                OptSearchButtonsMap.start = Calendar.getInstance();
-                OptSearchButtonsMap.start.set(year, month, day);
-                TVStart.setText(String.format("%4d-%02d-%02d", year, month + 1, day));
-            } else {
-                OptSearchButtonsMap.end = Calendar.getInstance();
-                OptSearchButtonsMap.end.set(year, month, day);
-                TVEnd.setText(String.format("%4d-%02d-%02d", year, month + 1, day));
-            }
+        if (this.ClickedBtnID == R.id.BtnSetDateStart) {
+            OptSearchButtonsMap.start = Calendar.getInstance();
+            OptSearchButtonsMap.start.set(year, month, day);
+            TVStart.setText(String.format("%4d-%02d-%02d", year, month, day));
 
-        } catch (Exception e) {
-            e.getStackTrace();
-            Toast.makeText(view.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+        } else {
+            OptSearchButtonsMap.end = Calendar.getInstance();
+            OptSearchButtonsMap.end.set(year, month, day);
+            TVEnd.setText(String.format("%4d-%02d-%02d", year, month, day));
         }
     }
+
 }
