6a30d41f8bf0ac7f47371e3f047d8cbf285b9b23
freshLiver <freshliver.cys@gmail.com>
2020-12-22 13:39:35 +0800

    add voice input button & add many comments & TODO: Thread crawler

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,21 +15,44 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected Button BntOptSearch, BtnPlayHistory;
+    /***********************************************************************
+     * DATA AND CONSTANTS
+     ***********************************************************************/
+    protected Button BtnOptSearch, BtnVoiceInput, BtnPlayHistory;
     protected EditText ETKeywords;
     protected TextView TVHistoryDate, TVHistoryContent;
 
+    /// 歷史上的今天相關變數
+    protected ArrayList<String[]> historyEvents;        // 所有歷史上的今天的事件，每個 element 為 String[Time,Intro]
+    protected int iHistory;                             // 當前顯示的是哪個事件（index）
+
+    /***********************************************************************
+     * Constructors/onCreate & get bundle & component settings
+     ***********************************************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get components and set their events
+        setComponents();
+
+        // init today's history
+        try {
+            this.historyEvents = null;
+            initHistory();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void setComponents() {
         // ! Find Components
-        this.BntOptSearch = findViewById(R.id.BtnOptSearch);
+        this.BtnOptSearch = findViewById(R.id.BtnOptSearch);
+        this.BtnVoiceInput = findViewById(R.id.BtnVoiceInput);
         this.BtnPlayHistory = findViewById(R.id.BtnPlayHistory);
         this.ETKeywords = findViewById(R.id.ETKeywords);
 
@@ -38,74 +60,101 @@ public class MainActivity extends AppCompatActivity {
         this.TVHistoryContent = findViewById(R.id.TVHistoryContent);
 
         // ! button clicked event
-        this.BntOptSearch.setOnClickListener(new MainButtonsMap(this));
+        this.BtnOptSearch.setOnClickListener(new MainButtonsMap(this));
         this.BtnPlayHistory.setOnClickListener(new MainButtonsMap(this));
 
         // ! history content clicked event
-        this.TVHistoryContent.setOnClickListener(v -> NewHistory(false));
-
-        // init today's history
-        InitHistory();
+        this.TVHistoryContent.setOnClickListener(v -> changeHistory(false));
     }
 
-    /**
-     * 從 this.ETKeywords 取得 keywords
-     *
-     * @return
-     */
-    public String getKeywords() {
-        return this.ETKeywords.getText().toString();
-    }
+    /***********************************************************************
+     * CLASS METHODS
+     ***********************************************************************/
 
     /**
-     * 歷史上的今天相關 method 以及 variables
+     * 獲取歷史上的今天的事件，放入 historyEvents 後會進行洗牌並選擇 index 0 的事件作為預設事件
      */
-    protected ArrayList<String[]> historyEvents;
-    protected int iHistory;
+    private void initHistory() throws InterruptedException {
 
-    private void InitHistory() {
+        Thread thWiki = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO get history event list
+
+//                ↓ SAMPLE CODE
+//                historyEvents = returnYourHistoryEventsHere();
+
+//                ↓ 預設要顯示的測試事件(需要先 new ArrayList<>() )
+//                historyEvents.add(new String[]{"8787", "8787878787"});
+//                historyEvents.add(new String[]{"666", "77777"});
+            }
+        });
+        thWiki.start();
+
+        // 等待爬蟲結束後才會進行洗牌以及更新 UI
+        thWiki.join();
+
+        // 將所有事件洗牌並設定當前要顯示的 index 為 0
         this.iHistory = 0;
-        this.historyEvents = null;
 
-        // TODO get history event list
-        this.historyEvents = new ArrayList<>();
-        this.historyEvents.add(new String[]{"8787", "8787878787"});
-        this.historyEvents.add(new String[]{"666", "77777"});
+        if (this.historyEvents != null)
+            Collections.shuffle(this.historyEvents);
 
-        // shuffle history events
-        Collections.shuffle(this.historyEvents);
-
-        // new a history
-        NewHistory(true);
+        // 更新 UI 以及 index
+        changeHistory(true);
     }
 
+    /**
+     * 切換到下一個歷史事件
+     *
+     * @param isInit 是否從 initHistory() 呼叫
+     */
     @SuppressLint({"DefaultLocale", "SetTextI18n"})
-    protected void NewHistory(boolean isInit) {
+    protected void changeHistory(boolean isInit) {
         // get today date
         Calendar today = Calendar.getInstance();
 
-        // if is not init increase history index
-        if (isInit == false)
+        // 如果不是正在初始化的話就需要 ++index (超出範圍後會回到 index 0)
+        if (isInit == false && this.historyEvents != null)
             this.iHistory = (this.iHistory + 1) % this.historyEvents.size();
 
-        // get new history
+        // history events 裡面有事件（not null）的話，就根據當前 index
         if (this.historyEvents != null) {
-            String dateInfo = String.format("西元 %4s / %02d / %02d "
-                    , this.historyEvents.get(this.iHistory)[0]
-                    , today.get(Calendar.MONTH)
-                    , today.get(Calendar.DAY_OF_MONTH));
 
+            // 根據 event 年份以及今天日期設定要顯示的日期
+            String dateInfo = String.format("西元 %4s / %02d / %02d "
+                    , this.historyEvents.get(this.iHistory)[0]                  // 獲得 event 的年份（數字用 %04d, 字串用 %4s）
+                    , today.get(Calendar.MONTH) + 1                             // 獲得今天月份（Calendar.MONTH 從 0 開始計算）
+                    , today.get(Calendar.DAY_OF_MONTH));                        // 獲得今天日期
             this.TVHistoryDate.setText(dateInfo);
+
+            // 將 TextView 的 text 設定為 event 的簡介
             this.TVHistoryContent.setText(this.historyEvents.get(this.iHistory)[1]);
         } else {
+
+            // 如果沒有事件的話則顯示無事件
             String dateInfo = String.format("西元 YYYY / %02d / %02d "
-                    , today.get(Calendar.MONTH)
+                    , today.get(Calendar.MONTH) + 1
                     , today.get(Calendar.DAY_OF_MONTH));
 
             this.TVHistoryDate.setText(dateInfo);
-            this.TVHistoryContent.setText("No Event (NULL)");
+            this.TVHistoryContent.setText("找不到歷史上的事件");
         }
         Toast.makeText(this, "下一則", Toast.LENGTH_SHORT).show();
+    }
+
+
+    /***********************************************************************
+     * GETTERS/SETTERS
+     ***********************************************************************/
+
+    /**
+     * 取得 EditText 中的文字作為關鍵字
+     *
+     * @return EditText.text
+     */
+    public String getKeywords() {
+        return this.ETKeywords.getText().toString();
     }
 }
 
@@ -122,6 +171,8 @@ class MainButtonsMap implements OnClickListener {
     @SuppressLint("NonConstantResourceId")
     public void onClick(View v) {
         switch (v.getId()) {
+
+            /// 搜尋
             case R.id.BtnOptSearch:
                 // bring keywords to next activity
                 Intent optSearch = new Intent(v.getContext(), OptionActivity.class);
@@ -134,10 +185,24 @@ class MainButtonsMap implements OnClickListener {
                 optSearch.putExtras(bundle);
                 this.main.startActivity(optSearch);
                 break;
+
+            /// 台語語音輸入
+            case R.id.BtnVoiceInput:
+                break;
+
+
+            /// 語音播放歷史
             case R.id.BtnPlayHistory:
-                // TODO 
-                String msg = "台語播放";
-                Toast.makeText(v.getContext(), msg, Toast.LENGTH_SHORT).show();
+                Thread thPlay = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TODO convert and play taiwanese here
+                    }
+                });
+
+                // start this thread
+                thPlay.start();
+
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
