f2201693a9ef9eb7bdd2d7eb13fd9577a0efca21
Frozn553 <jumbo33004@gmail.com>
2021-01-10 18:53:26 +0800

    Update Appledaily.java

--- app/src/main/java/com/example/finalproject/Appledaily.java
+++ app/src/main/java/com/example/finalproject/Appledaily.java
@@ -2,6 +2,9 @@ package com.example.finalproject;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.util.ArrayList;
 
 
 public class Appledaily extends News {
@@ -16,24 +19,42 @@ public class Appledaily extends News {
 
         try {
             // send get request and parse as html
-            Document doc_apple = Jsoup.connect(URL).get();
-
+            Document doc_apple = Jsoup.connect("https://tw.appledaily.com/local/20200703/PIEAD3K4RMRV4HYBZ3LVRGYX34").get();
+            
+            
             //get title
             title = doc_apple.select(".text_medium").text();
 
-
             //get date
             date = doc_apple.select(".timestamp").text();
-            date = date.substring(date.indexOf("更新時間： ") + 6);
-
-
+            if(date.indexOf("更新時間")!=-1) {
+            	date = date.substring(date.indexOf("更新時間") + 6);
+            }
             //get content
             content = doc_apple.select("p[class~=^text]").text();
-            content = content.substring(0, content.lastIndexOf("。") + 1);
+            ArrayList ad = new ArrayList();
+            Elements es = doc_apple.getElementsByTag("script");
+            for (Element child : es) {
+            	ad.add(child.toString());
+            }
+
+            String y = ad.get(17).toString();
+            int x = y.indexOf("raw_html");
+            if(x!=-1) {
+            	int t = y.indexOf("}", x+21);
+                y=y.substring(x+21, t);
+                y = y.replace("<br />", "");
+                y = y.replace("&nbsp;", "");
+                reporter = y;
+                y = y.substring(0, y.lastIndexOf("。") + 1);
+                content +=y;                
+            }else {
+            	reporter = content;
+                content = content.substring(0, content.lastIndexOf("。") + 1);
+            }
 
             //get reporter
-            reporter = doc_apple.select("p[class~=^text]").text();
-            reporter = reporter.substring(reporter.lastIndexOf("。") + 1, reporter.lastIndexOf("出版"));
+            reporter = reporter.substring(reporter.lastIndexOf("。") + 1, reporter.lastIndexOf("報導")+3);
 
         } catch (Exception e) {
             // TODO Auto-generated catch block
